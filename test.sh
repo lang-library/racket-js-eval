a0c8fce064c769e8fe3afbb13ac9cc1cf1783344
javacommons <javacommons@gmail.com>
2026-03-24 06:01:47 +0900

    .

--- test.sh
+++ test.sh
@@ -5,15 +5,15 @@ set -e
 #raco exe -o test.exe test.rkt
 raco exe --gui -o test.exe test.rkt
 
-rcedit-x64.exe ./test.exe --set-icon                           "./mingw32.ico"
+rcedit-x64.exe ./test.exe --set-icon                                              "./mingw32.ico"
 
-rcedit-x64.exe ./test.exe --set-version-string FileDescription "test"
-rcedit-x64.exe ./test.exe --set-version-string ProductName     "test"
-rcedit-x64.exe ./test.exe --set-version-string CompanyName     "JavaCommons Technologies"
+rcedit-x64.exe ./test.exe --set-version-string FileDescription  "test"
+rcedit-x64.exe ./test.exe --set-version-string ProductName    "test"
+rcedit-x64.exe ./test.exe --set-version-string CompanyName "JavaCommons Technologies"
 rcedit-x64.exe ./test.exe --set-version-string ProductVersion  "1.0.0"
-rcedit-x64.exe ./test.exe --set-version-string FileVersion     "1.0.0"
+rcedit-x64.exe ./test.exe --set-version-string FileVersion         "1.0.0"
 rcedit-x64.exe ./test.exe --set-version-string LegalCopyright  "Copyright JavaCommons Technologies"
 
 rm -rf inst.tmp
 raco distribute inst.tmp test.exe || true
-vbpack.exe -i inst.tmp/test.exe -o ./test.exe
+exepack.exe -i inst.tmp/test.exe -o ./test-packed.exe
